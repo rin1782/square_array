634076c1c12297478ed2e065d437c854b744381a
Erin Cola <colaerin@yahoo.com>
2020-11-16 18:11:13 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+
 def square_array(array)
-  # your code here
+  new_array = []
+    array.each do |number|
+    new_array << number ** 2 
+  end
+  return new_array
 end
+
+# 1) Create a new array 
+# 2)Square each element in the array and << 
+# each of those squared elements into that new array that you made in step 1) 
+# 3) return that new array with 
+# those squared elements stored in it
